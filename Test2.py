67cac63b63c7fb8781b0a528dc6091eabf0d087d
Karnpreet Grewal <karnpreetgrewal@Karnpreets-MacBook-Air.local>
2021-04-29 20:12:20 -0700

    commit

--- Test2.py
+++ Test2.py
@@ -1,1 +1,11 @@
-import Final.txt
+"""
+
+This program is ment to show the number of grades above the class avarger. It will also show the average grade of those students that scored above class average. Then we will 
+"""
+
+"""
+
+Peusodo code 
+
+
+"""
