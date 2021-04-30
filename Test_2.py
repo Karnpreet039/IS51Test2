f2a77d1b7904398b6546a832cc1ec892af4db969
Karnpreet Grewal <karnpreetgrewal@Karnpreets-MacBook-Air.local>
2021-04-29 23:38:24 -0700

    finnish exam-end

--- Test_2.py
+++ Test_2.py
@@ -21,3 +21,24 @@ print (average grade)
 print(Precentage of grade above average)
 
 """
+
+
+def main():
+    finaldict = create.dictionary("Final.txt")
+    displayinfoavoutfinalgrades(finaldict)
+
+infile = open("Final.txt", 'r')
+grades = [line.rstrip() for line in infile]
+infile.close()
+
+for i in range(len(grades)):
+    grades [i] = int(grades [i])
+average = sum(grades) / len(grades)
+num = 0
+for grade in grades:
+    if grade>average:
+        num += 1
+print("Number of grades:", len(grades))
+print("Average grade:", average)
+print("Percentage of grades above average: {0:.2f}%".format(100 * num / len(grades)))
+
